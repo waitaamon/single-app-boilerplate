1fb0b7bb72f11f385d33a75c16d72ae1cd6d0887
Amon Gathoka <waitaamon@gmail.com>
2017-08-01 09:00:57 +0300

    Initial Commit

---
+++ app/Http/Controllers/Account/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Account;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegisterController extends Controller
+{
+    public function register(){
+
+    }
+}
